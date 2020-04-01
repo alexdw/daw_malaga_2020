b2125d5edde83c766c2cf988309d373aa26f2d00
Espe <ego.egarco01@gmail.com>
2020-04-01 22:17:46 +0200

    katas

--- students/esperanza_garcia/katas/practices/02_bob_lazy_man/src/main.js
+++ students/esperanza_garcia/katas/practices/02_bob_lazy_man/src/main.js
@@ -1,13 +1,21 @@
 export function countLettersAndDigits(input) {
-  let total=[];
-  let longitudInput = input.lenght;
-  for(let i =0; i<longitudInput;i++){
-     var letters = /^[A-Za-z]+$/
-     if(input.charAt(i)!==NaN || input.charAt(i)===letters ){
-        total.push(input.charAt(i))
-     }
-     return total.lenght 
-  }
-  
-  return total.lenght 
+   let total=[];
+   let numero= input.length;
+   for(let i=0; i<numero; i++){
+      if(input.charCodeAt(i)>= 48 && 
+         input.charCodeAt(i)<=57){
+            total.push(input.charCodeAt(i))
+         }
+      if(input.charCodeAt(i)>= 65 && 
+         input.charCodeAt(i)<=90){
+            total.push(input.charCodeAt(i))
+         } 
+      if(input.charCodeAt(i)>= 97 && 
+         input.charCodeAt(i)<=122){
+            total.push(input.charCodeAt(i))
+         }
+         
+   }
+   return total.length
 }
+ 
