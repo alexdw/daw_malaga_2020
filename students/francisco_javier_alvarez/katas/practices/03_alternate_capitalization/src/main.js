bed49b462237faa548ca3eec2d5014519cb25191
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-04-01 19:53:31 +0200

    añado 03_alternate_capitalization

---
+++ students/francisco_javier_alvarez/katas/practices/03_alternate_capitalization/src/main.js
@@ -0,0 +1,19 @@
+export function capitalize(s) {
+  let cadena1 = Array.from(s, (item, i) => {
+      if(i%2===0) {
+        return item.toUpperCase();
+      }
+      return item;
+  }).join("");
+
+  let cadena2 = Array.from(s, (item,i) => {
+      if (i%2!==0) {
+          return item.toUpperCase();
+      }
+      return item;
+  }).join("");
+
+  let resultado = [cadena1, cadena2];
+
+  return resultado;
+}
