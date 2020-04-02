71dab3ac1a7eb965c3142d3da5265dd599079b48
Álex Martín <alex@alexdw.com>
2020-04-02 11:48:04 +0200

    Katas resolved

--- katas/practices_resolved/04_sum_of_minimuns/src/main.js
+++ katas/practices_resolved/04_sum_of_minimuns/src/main.js
@@ -1,3 +1,15 @@
+// [
+// 	[7, 9, 8, 6, 2], 
+// 	[6, 3, 5, 4, 3], 
+// 	[5, 8, 7, 4, 5]
+// ]
+
+function compare(a,b){
+	return a - b
+}
+
 export const sumOfMinimums = (arr) => {
-   
+   let arrayMapped = arr.map( value => value.sort(compare)[0] );
+   let total = arrayMapped.reduce( (acummulated, current) => acummulated + current);
+   return total;
 }
