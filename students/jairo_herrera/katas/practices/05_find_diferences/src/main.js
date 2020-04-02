5e0e18668bffb05e0dbc0b845d97eb28715a7827
jaaiiriilloo <jaaiiriilloo@gmail.com>
2020-04-02 23:18:29 +0200

    kata-5

--- students/jairo_herrera/katas/practices/05_find_diferences/src/main.js
+++ students/jairo_herrera/katas/practices/05_find_diferences/src/main.js
@@ -1,3 +1,7 @@
 export const differenceInAges = (ages) => {
-    
+let minAge = Math.min(...ages);
+let maxAge = Math.max(...ages)
+let diffAge = maxAge - minAge;
+
+  return (minAge,maxAge,diffAge)
 }
--- students/jairo_herrera/katas/practices/06_weird_case/src/main.js
+++ students/jairo_herrera/katas/practices/06_weird_case/src/main.js
@@ -1,3 +1,17 @@
 export const toWeirdCase = (text) => {
-   return ""
+  
+   let words = text.split(" ");
+   let wordsWeird = words.map( word => weird(word) );
+  
+   return weird (text)
+}
+
+function weird(word) {
+   let letters = Array.from(word);
+   let lettersWeird = letters.map( (letter, index)=> {
+      if(index%2===0) return letter.toUpperCase();
+      return letter;
+   })
+  return lettersWeird.join("");
+   // console.log(">>>>>", letters)
 }
