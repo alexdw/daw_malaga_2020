f6e4d93bf3e6b384c278eb69d33a2cb36fae25fe
jaaiiriilloo <jaaiiriilloo@gmail.com>
2020-04-02 22:47:43 +0200

    test-kata4

--- students/jairo_herrera/katas/practices/04_sum_of_minimuns/src/main.js
+++ students/jairo_herrera/katas/practices/04_sum_of_minimuns/src/main.js
@@ -1,3 +1,16 @@
+
+function compare(a,b){
+  return a - b
+}
+
 export const sumOfMinimums = (arr) => {
-   
+ let arrayMapped = arr.map(value => value.sort(compare)[0] );
+ let total = arrayMapped.reduce (
+   (acummulated, current) => {
+     return acummulated + current
+   }
+
+ );
+ return total;
+ 
 }
