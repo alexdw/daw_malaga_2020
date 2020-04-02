f04a869668b7105408260447f106a244abc3f89a
jaaiiriilloo <jaaiiriilloo@gmail.com>
2020-04-02 21:30:15 +0200

    practices/01_trolling_troll

---
+++ students/jairo_herrera/katas/js_avanzado/arrow_context/src/main.js
@@ -0,0 +1,31 @@
+/**
+ * Resuelve el ámbito del método startNoise con arrow functions
+ */
+
+export const init = () => {
+
+    class Animal{
+        constructor(name,sound){
+            this.name = name;
+            this.sound = sound
+        }
+
+        makeNoise() {
+            return this.sound;
+        }
+
+        startNoise() {
+        
+            return new Promise(function(resolve) {
+                resolve(this.makeNoise());
+            });
+
+        }
+
+    }
+    
+    let cat = new Animal("Kitty", "meaaaaaw");
+    let sound = cat.startNoise();
+
+    return sound;
+}
